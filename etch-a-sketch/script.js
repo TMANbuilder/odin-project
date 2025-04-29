a1bac5f78b9de5d833eca3004cea38e8e0253973
Taryn Charters <chartestaryn@gmail.com>
2025-04-29 13:19:53 -0400

    initializing etch-a-sketch project: able to generate grid. Working through bugs related to grid

---
+++ etch-a-sketch/script.js
@@ -0,0 +1,40 @@
+function createGrid(targetValue) {
+
+    gridSize = Number(targetValue);
+
+    console.log(`gridSize is: ${gridSize}`);
+    console.log(`gridSize type is: ` + typeof(gridSize));
+
+    //Validate gridSize is a number
+    if (Number.isInteger(gridSize) == false) {
+        console.log("gridSize is not a integer");
+        return;
+    }
+
+    const gridContainer = document.getElementById("grid");
+    gridContainer.innerHTML = "";
+
+    for (let i = 0; i < gridSize; i++) {
+
+        console.log(`OUTER: Cycle count: ${i}`);
+        const gridRow = document.createElement("div");
+        gridRow.className = "grid-column";
+
+        for (let y = 0; y < gridSize; y++) {
+
+            console.log(`INNER: Cycle count: ${y}`);
+
+            const gridCell = document.createElement("div");
+            gridCell.className = "grid-cell";
+            gridCell.innerText = "-";
+            
+            gridRow.appendChild(gridCell);
+
+        }
+
+        gridContainer.appendChild(gridRow);
+
+    };
+
+
+}

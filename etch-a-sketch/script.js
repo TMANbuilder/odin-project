8ffb692aa5b989c93b483194303963e7ebc0f3bb
TMAN <charterstaryn@gmail.com>
2025-04-29 21:09:56 -0400

    completed 'clear grid' functionality

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -2,12 +2,8 @@ function createGrid(targetValue) {
 
     gridSize = Number(targetValue);
 
-    console.log(`gridSize is: ${gridSize}`);
-    console.log(`gridSize type is: ` + typeof(gridSize));
-
-    //Validate gridSize is a number
+    // validate gridSize is a number
     if (Number.isInteger(gridSize) == false) {
-        console.log("gridSize is not a integer");
         return;
     }
 
@@ -47,3 +43,7 @@ function createGrid(targetValue) {
 
 };
 
+function clearGrid() {
+    const gridContainer = document.getElementById("grid");
+    gridContainer.innerHTML = "";    
+}

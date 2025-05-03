579990ab8ea873e12ea68a4815806850d0b87198
TMAN <charterstaryn@gmail.com>
2025-05-03 14:16:52 -0400

    introduced functionality to get message from user, count word frequency and return hash with results

--- sub-strings/main.rb
+++ sub-strings/main.rb
@@ -13,12 +13,20 @@ class SubString
   end
 
   def main
+    results = {}
     puts "Welcome to the Substring Tool"
     puts "This tools counts the number of times a word appears in a line of text"
     puts "Start by typing a line of text in the input field"
-    setMessage gets.chomp 
+    setMessage gets.chomp
+    array = getMessage.split(" ")
+    array.each {
+      |word|
+      results[word] = array.count(word)
+    }    
+    results
   end
 end
 
 run = SubString.new
-run.main
+message = run.main
+puts "String count is:  #{message}"
